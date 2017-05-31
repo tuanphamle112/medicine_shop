ca5936deb08446449558046e8fa324ba969456e6
Pham Le Tuan <tuanphamle112@gmail.com>
2017-05-31 16:24:25 +0700

    detail-rating2

---
+++ database/migrations/2017_05_31_162251_modify_rate_medicine_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyRateMedicineTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rate_medicines', function(Blueprint $table) {
+            $table->float('point_rate')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
