f5f10c93ccff27f75a641149f603ecfd425fec1e
dinhvantai <tai.dinhvan2702@gmail.com>
2017-07-03 09:01:25 +0700

    Edit Pull Tuan

--- public/js/template_medicio/custom.js
+++ public/js/template_medicio/custom.js
@@ -1,94 +1,91 @@
 /*global jQuery:false */
 (function ($) {
 
-	var wow = new WOW(
-	  {
-		boxClass:     'wow',      // animated element css class (default is wow)
-		animateClass: 'animated', // animation css class (default is animated)
-		offset:       0,          // distance to the element when triggering the animation (default is 0)
-		mobile:       false       // trigger animations on mobile devices (true is default)
-	  }
-	);
-	wow.init();
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       false       // trigger animations on mobile devices (true is default)
+      }
+    );
+    wow.init();
 
-	//jQuery to collapse the navbar on scroll
-	
-	//scroll to top
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollup').fadeIn();
-			} else {
-			$('.scrollup').fadeOut();
-		}
-	});
-	$('.scrollup').click(function(){
-		$("html, body").animate({ scrollTop: 0 }, 1000);
-			return false;
-	});
-	
+    //jQuery to collapse the navbar on scroll
+    
+    //scroll to top
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+            } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $(document).on('click', '.scrollup', function() {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
 
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.navbar-nav li a').bind('click', function(event) {
+            var $anchor = $(this);
+            var nav = $($anchor.attr('href'));
+            if (nav.length) {
+            $('html, body').stop().animate({                
+                scrollTop: $($anchor.attr('href')).offset().top                
+            }, 1500, 'easeInOutExpo');
+            
+            event.preventDefault();
+            }
+        });
+        $('.page-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
-	//jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$('.navbar-nav li a').bind('click', function(event) {
-			var $anchor = $(this);
-			var nav = $($anchor.attr('href'));
-			if (nav.length) {
-			$('html, body').stop().animate({				
-				scrollTop: $($anchor.attr('href')).offset().top				
-			}, 1500, 'easeInOutExpo');
-			
-			event.preventDefault();
-			}
-		});
-		$('.page-scroll a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
-
-	//owl carousel
-	$('#owl-works').owlCarousel({
+    //owl carousel
+    $('#owl-works').owlCarousel({
             items : 4,
             itemsDesktop : [1199,5],
             itemsDesktopSmall : [980,5],
             itemsTablet: [768,5],
             itemsTabletSmall: [550,2],
             itemsMobile : [480,2],
-	});
-	
-	//nivo lightbox
-	$('.owl-carousel .item a').nivoLightbox({
-		effect: 'fadeScale',                             // The effect to use when showing the lightbox
-		theme: 'default',                           // The lightbox theme to use
-		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
-		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
-		onInit: function(){},                       // Callback when lightbox has loaded
-		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
-		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
-		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
-		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
-		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
-		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
-		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
-	});
+    });
+    
+    //nivo lightbox
+    $('.owl-carousel .item a').nivoLightbox({
+        effect: 'fadeScale',                             // The effect to use when showing the lightbox
+        theme: 'default',                           // The lightbox theme to use
+        keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+        clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+        onInit: function(){},                       // Callback when lightbox has loaded
+        beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+        afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+        beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+        afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+        onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+        onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+        errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+    });
 
-	jQuery('.appear').appear();
-	jQuery(".appear").on("appear", function(data) {
-			var id = $(this).attr("id");
-			jQuery('.nav li').removeClass('active');
-			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
-		});
-
-		
-		//parallax
+    jQuery('.appear').appear();
+    jQuery(".appear").on("appear", function(data) {
+            var id = $(this).attr("id");
+            jQuery('.nav li').removeClass('active');
+            jQuery(".nav a[href='#" + id + "']").parent().addClass("active");                    
+        });
+        
+        //parallax
         if ($('.parallax').length)
         {
-			$(window).stellar({
-				responsive:true,
+            $(window).stellar({
+                responsive:true,
                 scrollProperty: 'scroll',
                 parallaxElements: false,
                 horizontalScrolling: false,
@@ -97,14 +94,13 @@
             });
 
         }
-		
-		
+        
 (function ($, window, document, undefined) {
 
     var gridContainer = $('#grid-container'),
         filtersContainer = $('#filters-container');
 
-	// init cubeportfolio
+    // init cubeportfolio
     gridContainer.cubeportfolio({
 
         defaultFilter: '*',
@@ -195,12 +191,10 @@
     gridContainer.cubeportfolio('showCounter', filtersContainer.find('.cbp-filter-item'));
 
 })(jQuery, window, document);
-		
-	
+        
+    
 })(jQuery);
 $(window).load(function() {
-	$(".loader").delay(100).fadeOut();
-	$("#page-loader").delay(100).fadeOut("fast");
+    $(".loader").delay(100).fadeOut();
+    $("#page-loader").delay(100).fadeOut("fast");
 });
-
-	
